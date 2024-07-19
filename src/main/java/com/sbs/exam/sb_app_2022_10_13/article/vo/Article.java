b850922ed470dbcf5d643796f6b2c2067da3ae8f
waitiaw <xx10005xx@naver.com>
2024-07-19 10:12:19 +0900

    에러 정정

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/article/vo/Article.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/article/vo/Article.java
@@ -8,7 +8,38 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Article {
-  private int id;
-  private String title;
-  private String body;
+  public int id;
+  public String title;
+  public String body;
+  public Article() {}
+
+  public Article(int id, String title, String body) {
+    this.id = id;
+    this.title = title;
+    this.body = body;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getBody() {
+    return body;
+  }
+
+  public void setBody(String body) {
+    this.body = body;
+  }
 }
